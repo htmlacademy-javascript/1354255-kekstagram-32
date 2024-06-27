7a1935eb0dc1ddc8b8f54c354b757ed5284d9d2a
Mars <ilon.mars@yandex.ru>
2024-06-27 19:49:51 +0300

    add: function for check if meeting fits workday

---
+++ js/date-functions.js
@@ -0,0 +1,47 @@
+const getMinutesFromDayStart = (time) => {
+  const MINUTES_IN_HOUR = 60;
+
+  const [hours, minutes] = time.split(':').map((string) => parseInt(string, 10));
+  return hours * MINUTES_IN_HOUR + minutes;
+};
+
+export const checkMeetingFitsSchedule = (workdayStart, workdayEnd, meetingStart, meetingDuration) => {
+  const minutesFromWorkdayStart = getMinutesFromDayStart(workdayStart);
+  const minutesFromWorkdayEnd = getMinutesFromDayStart(workdayEnd);
+  const minutesFromMeetingStart = getMinutesFromDayStart(meetingStart);
+
+  if (minutesFromMeetingStart < minutesFromWorkdayStart || minutesFromMeetingStart >= minutesFromWorkdayEnd) {
+    return false;
+  }
+
+  return (minutesFromMeetingStart + meetingDuration) <= minutesFromWorkdayEnd;
+};
+
+/*
+* Можно еще сделать через создание даты из времени, технически это работает также,
+* только возвращаются прошедшие мс с самой первой даты, а не минуты с начала дня
+* в каком-то смысле это даже какое-то усложнение
+*/
+
+const getDateFromTime = (time) => {
+  const dateTime = new Date();
+  const [hours, minutes] = time.split(':');
+
+  return dateTime.setHours(hours, minutes);
+};
+
+export const checkMeetingFitsWorkday = (workdayStart, workdayEnd, meetingStart, meetingDuration) => {
+  const MS_IN_SECOND = 1000;
+  const SECONDS_IN_MINUTE = 60;
+
+  const workdayStartTime = getDateFromTime(workdayStart);
+  const workdayEndTime = getDateFromTime(workdayEnd);
+  const meetingStartTime = getDateFromTime(meetingStart);
+
+
+  if (meetingStartTime < workdayStartTime || meetingStartTime >= workdayEndTime) {
+    return false;
+  }
+
+  return meetingStartTime + meetingDuration * SECONDS_IN_MINUTE * MS_IN_SECOND <= workdayEndTime;
+};
