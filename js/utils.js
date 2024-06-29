a7eb243c8abb4b71d819263f0e6d268e6a02b30e
Mars <ilon.mars@yandex.ru>
2024-06-29 13:22:43 +0300

    test: create tests for createPhotos

--- js/utils.js
+++ js/utils.js
@@ -1,41 +1,3 @@
-/* eslint no-console: 0 */
-
-const RESET_CSS_ATTRIBUTE = '\x1B[m';
-
-const getConsoleGroupMessage = (groupName) => [
-  `%c ${groupName}${RESET_CSS_ATTRIBUTE} tests`,
-  'color: cornflowerblue;',
-];
-
-const testResult = (testValue, expectedResult) => {
-  const SUCCESS_COLOR = 'color: #bada55';
-  const ERROR_COLOR = 'color: #dc143c';
-
-  const isEqual =
-    testValue === expectedResult ||
-    (Number.isNaN(testValue) && Number.isNaN(expectedResult));
-
-  const testMessage = `%cTest ${
-    isEqual ? 'passed' : 'failed'
-  }${RESET_CSS_ATTRIBUTE}`;
-  const textColor = isEqual ? SUCCESS_COLOR : ERROR_COLOR;
-
-  console.log(
-    `${testMessage}\nExpecting - %c${expectedResult}${RESET_CSS_ATTRIBUTE}, getting - %c${testValue}`,
-    `background: #222; ${textColor}; padding: 3px;`,
-    SUCCESS_COLOR,
-    textColor
-  );
-};
-
-export const testCases = ({ message, cb, cases }) => {
-  console.group(...getConsoleGroupMessage(message || cb.name));
-  cases.forEach(({ values, expectedResult }) => {
-    testResult(cb(...values), expectedResult);
-  });
-  console.groupEnd();
-};
-
 export const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -51,6 +13,7 @@ export const createRandomIdFromRangeGenerator = (min, max) => {
     let currentValue = getRandomInteger(min, max);
 
     if (previousValues.length >= (max - min + 1)) {
+      // eslint-disable-next-line no-console
       console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
       return null;
     }
