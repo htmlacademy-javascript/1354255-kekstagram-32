4ffc985aecacabae29e42098122f152944d9e72e
Mars <ilon.mars@yandex.ru>
2024-08-01 21:07:11 +0300

    remove: unused code

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -12,18 +12,3 @@ export function debounce(callback, timeoutDelay = 500) {
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 }
-
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
-export function throttle (callback, delayBetweenFrames) {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-}
