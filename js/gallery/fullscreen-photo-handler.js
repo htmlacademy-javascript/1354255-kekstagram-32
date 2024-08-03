7b5ffb0888aac67ff26c99e5994d49c463c0dad4
Mars <ilon.mars@yandex.ru>
2024-08-03 21:27:59 +0300

    fix: tests

--- js/gallery/fullscreen-photo-handler.js
+++ js/gallery/fullscreen-photo-handler.js
@@ -1,6 +1,6 @@
 import { openModal } from '../modal-plugin.js';
 import { getPhotoById, ModalEnum } from '../utils';
-import { renderFullscreenPhoto } from './render-fullscreen-photo.js';
+import { renderFullscreenPhoto, resetFullscreenPhoto } from './render-fullscreen-photo.js';
 
 export const fullscreenPhotoHandler = (photos, photosContainerElement) => {
   const pictureModalElement = document.querySelector('.big-picture');
@@ -15,6 +15,10 @@ export const fullscreenPhotoHandler = (photos, photosContainerElement) => {
 
     const photo = getPhotoById(picture.dataset.id, photos);
     renderFullscreenPhoto(photo);
-    openModal(ModalEnum.FULLSCREEN_PHOTO, { modalElement: pictureModalElement, closeButtonElement });
+    openModal(ModalEnum.FULLSCREEN_PHOTO, {
+      closeButtonElement,
+      modalElement: pictureModalElement,
+      beforeCloseCallback: resetFullscreenPhoto
+    });
   });
 };
